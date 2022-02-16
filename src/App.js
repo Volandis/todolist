e2d1d3dc269b406d8dd447f340b942115771b445
Volandis <95025606+Volandis@users.noreply.github.com>
2022-02-16 16:53:34 +0000

    update after css
    updated css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./index.css";
 
-function App() {
+const App = () => {
+
+  const [newTask, setNewTask] = useState("");
+  const [newTasks, setNewTasks] = useState([]);
+
+  const handleChange = e => {
+    setNewTask(e.target.value);
+  };
+
+  const addNewTask = () => {
+    setNewTasks([
+      ...newTasks,
+      {
+        id: newTasks.length + 1,
+        text: newTask,
+        completed: false
+      }
+    ]);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (newTask === "") return;
+    addNewTask();
+    setNewTask("");
+  };
+
+  const removeNewTask = newTaskId => {
+    const updatedNewTasks = newTasks.filter(newTask => newTask.id !== newTaskId);
+    setNewTasks(updatedNewTasks);
+  };
+
+  const toggleNewTask = newTaskId => {
+    const updatedNewTasks = newTasks.map(newTask => {
+      return newTask.id === newTaskId
+        ? { ...newTask, completed: !newTask.completed }
+        : newTask;
+    });
+
+    setNewTasks(updatedNewTasks);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <form onSubmit={onSubmit}>
+        <h1>To Do List</h1>
+
+        <br />
+
+        <input
+          placeholder="New task details"
+          id="newTask"
+          className="newTask-input"
+          onChange={handleChange}
+          value={newTask}
         >
-          Learn React
-        </a>
-      </header>
+        </input>
+
+        <br />
+
+        <button type="submit" className="addTaskButton">Add Task</button>
+
+      </form>
+
+      <br />
+
+      <div>
+        <ul className="ulList">
+          {newTasks.map(newTask => (
+          <div className="list">  
+            <li key={newTask.id} className="liList">
+
+              <div
+                className={newTask.completed ? "newTask-completed" : undefined}
+                onClick={() => toggleNewTask(newTask.id)}
+                >
+                {newTask.text}
+              </div>
+
+            </li>
+
+            <button className="delete-btn" onClick={() => removeNewTask(newTask.id)}>
+              delete
+            </button>
+
+          </div>
+          ))}
+
+        </ul>
+
+      </div>
+
     </div>
   );
 }
